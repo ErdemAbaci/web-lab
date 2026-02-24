e5ff1e857823cb30dcf6ba66e84b16710ba58608
Erdem <erdemootv1@gmail.com>
2026-02-24 15:05:58 +0300

    docs: add compresenhive README

--- App.tsx
+++ App.tsx
@@ -3,10 +3,10 @@ function App() {
     <div>
       <h1>Web Tasarimi ve Programlama</h1>
       <h2>LAB-1</h2>
-      <p>Ad Soyad: [Mehmet Erdem Abacı]</p>
-      <p>Ogrenci No: [235541043]</p>
-      <p>Hobi: [Okumak]</p>
-      <p>Bölüm Adı: [Yazılım Mühendisliği]</p>
+      <p>Ad Soyad:Mehmet Erdem Abacı</p>
+      <p>Ogrenci No: 235541043</p>
+      <p>Hobi: Okumak</p>
+      <p>Bölüm Adı: Yazılım Mühendisliği</p>
     </div>
   )
 }
